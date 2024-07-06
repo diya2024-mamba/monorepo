1d23f229651209ab3b830a3b18193c0e7cdea187
kukjinkim <ye20013@gmail.com>
2024-07-06 09:57:35 +0900

    first commit

---
+++ projects/rl/pytorch/modules/networks/actor_critic_agnostic.py
@@ -0,0 +1,338 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal, Categorical
+from omegaconf import OmegaConf
+from modules.networks.mlp import ResidualMLP
+from modules.utils import get_activation
+from gymnasium.spaces import Box as GymnasiumBox
+from gymnasium.spaces import Discrete as GymnasiumDiscrete
+from gymnasium.core import Env as GymnasiumEnv
+from mamba_ssm import Mamba
+import math
+from typing import TypeVar, List, Tuple
+GymSpace = TypeVar('GymSpace', GymnasiumBox, GymnasiumDiscrete)
+Env = GymnasiumEnv
+
+
+class MambaEncoder(nn.Module):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                ):
+        super().__init__()
+        activation_name: str = cfg.nn.actor_critic.activation
+        self.d_model = cfg.nn.actor_critic.d_model
+        self.act_func: torch.nn.Module = get_activation(activation_name)()
+        d_state = cfg.nn.mamba.d_state
+        d_conv = cfg.nn.mamba.d_conv
+        expand = cfg.nn.mamba.expand
+        num_blocks = cfg.nn.mamba.num_blocks
+        self.blocks = nn.Sequential()
+        self.embedding = nn.Linear(1, self.d_model)
+        for i in range(num_blocks):
+            mamba_block = Mamba(d_model=self.d_model, # Model dimension d_model
+                                    d_state=d_state,  # SSM state expansion factor
+                                    d_conv=d_conv,    # Local convolution width
+                                    expand=expand,    # Block expansion factor
+                                ).cuda()
+            activation = self.act_func
+            self.blocks.append(mamba_block)
+            self.blocks.append(activation)
+    
+    def forward(self, x):
+        # x: [batch, feature_dim]
+        ux = x.unsqueeze(-1)
+        ux = self.embedding(ux)
+        hidden = self.blocks(ux)
+        return hidden
+        
+class MambaDecoder(nn.Module):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                ):
+        super().__init__()
+        activation_name: str = cfg.nn.actor_critic.activation
+        self.d_model = cfg.nn.actor_critic.d_model
+        self.act_func: torch.nn.Module = get_activation(activation_name)()
+        d_state = cfg.nn.mamba.d_state
+        d_conv = cfg.nn.mamba.d_conv
+        expand = cfg.nn.mamba.expand
+        num_blocks = cfg.nn.mamba.num_blocks
+        self.blocks = nn.Sequential()
+        for i in range(num_blocks):
+            mamba_block = Mamba(d_model=self.d_model, # Model dimension d_model
+                                    d_state=d_state,  # SSM state expansion factor
+                                    d_conv=d_conv,    # Local convolution width
+                                    expand=expand,    # Block expansion factor
+                                ).cuda()
+            activation = self.act_func
+            self.blocks.append(mamba_block)
+            self.blocks.append(activation)
+        self.blocks.pop(-1)
+
+    def forward(self, 
+                out_dim:int,
+                hidden:torch.Tensor):
+        # x: [batch, out_dim, hidden_dim]
+        batch_size = hidden.size(0)
+        h = hidden.unsqueeze(1) 
+        h = h.expand(batch_size, out_dim, self.d_model)
+        # if self.pos_encoding:
+            # h = positional_encoding(h, self.d_model, out_dim)
+        output = self.blocks(h)
+        return output
+
+class AgnosticBase(nn.Module):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                env_list,
+                 ):
+        super().__init__()
+        self.d_model = cfg.nn.actor_critic.d_model
+        activation_name: str = cfg.nn.actor_critic.activation
+        self.act_func: torch.nn.Module = get_activation(activation_name)()
+        self.obs_encoder_1d: torch.nn.Module = MambaEncoder(cfg)
+        self.input_to_hidden: bool = cfg.nn.actor_critic.input_to_hidden
+        self.hidden_to_output: bool = cfg.nn.actor_critic.hidden_to_output
+        self.use_mlp: bool = cfg.nn.actor_critic.use_mlp
+        if self.input_to_hidden == "mean":
+            self.in_hidden_op = torch.mean
+        elif self.in_hidden_op == "sum":
+            self.in_hidden_op = torch.sum
+        if self.hidden_to_output == "mean":
+            self.out_hidden_op = torch.mean
+        elif self.hidden_to_output == "sum":
+            self.out_hidden_op = torch.sum
+        if self.use_mlp:
+            self.res_mlp: torch.nn.Module = ResidualMLP(cfg, self.d_model, self.d_model)
+        self.env_ids = env_ids[:]
+
+    def encoding(self,
+                 env: Env,
+                 x: torch.Tensor):
+        env_id = env.env_id
+        # ? Encoding from 1D 
+        # x: [batch_size, feature_dim]
+        h = self.obs_encoder_1d(x)
+        h = self.in_hidden_op(h, dim=1, keepdim=False)
+        if self.use_mlp:
+            h = self.act_func(self.res_mlp(h))
+        else:
+            h = self.act_func(h)
+        return h
+    
+    def decoding(self):
+        pass
+    
+    def forward(self):
+        pass
+        
+class AgnosticStochasticActor(AgnosticBase):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                 env_list: List[Env],
+                 ):
+        super().__init__(cfg, env_ids, env_list)
+        self.policy_mean_decoder: torch.nn.Module = MambaDecoder(cfg)
+        self.policy_logstd_decoder: torch.nn.Module = MambaDecoder(cfg)
+        self.policy_prob_decoder: torch.nn.Module = MambaDecoder(cfg)
+
+    def decoding(self,
+                 action_space: GymSpace,
+                 h: torch.Tensor):
+        # ? Decoding continuous action
+        if isinstance(action_space, GymnasiumBox):
+            act_dim = np.prod(action_space.shape)
+            a_mean = self.policy_mean_decoder(act_dim, h)
+            a_logstd = self.policy_logstd_decoder(act_dim, h)
+            self.a_mean_weights = a_mean
+            self.a_logstd_weights = a_logstd
+            # a_mean, a_logstd: [batch_size, act_dim, d_model]
+            a_mu = self.out_hidden_op(a_mean, dim=2, keepdim=False) # out: [batch_size, act_dim]
+            a_logstd = self.out_hidden_op(a_logstd, dim=2, keepdim=False) # out: [batch_size, act_dim]
+            actor_std = F.softplus(a_logstd)
+            dist = Normal(a_mu, actor_std)
+            return dist, a_mu
+
+        # ? Decoding discrete action
+        elif isinstance(action_space, GymnasiumDiscrete):
+            # get num_discretes
+            num_discretes = action_space.n
+            # generate policy weights
+            a_probs = self.policy_prob_decoder(num_discretes, h)
+            self.a_prob_weights = a_probs
+            logits = self.out_hidden_op(a_probs, dim=2, keepdim=False)
+            # get categorical distribution
+            dist = Categorical(logits=logits)
+            return dist, logits
+    
+    def forward(self, 
+                env: Env,
+                x: torch.Tensor):
+        action_space = env.single_action_space
+        # ? Encoding
+        # h: [batch_size, d_model]
+        h = self.encoding(env, x)
+        
+        # ? Decoding
+        dist, _ = self.decoding(action_space, h)
+        return dist, _
+
+        
+class AgnosticVNetwork(AgnosticBase):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                 env_list: List[Env],
+                 ):
+        super().__init__(cfg, env_ids, env_list)
+        self.value_decoder: torch.nn.Module = MambaDecoder(cfg)
+
+    def decoding(self, 
+                 h:torch.Tensor):
+        value = self.value_decoder(1, h)
+        self.value_weights = value 
+        value = self.out_hidden_op(value, dim=2, keepdim=False) 
+        return value
+        
+    def forward(self, 
+                env: Env,
+                x:torch.Tensor):
+        # x: [batch_size, feature_dim]
+        # ? Encoding
+        h = self.encoding(env, x)
+        # h: [batch_size, d_model]
+    
+        # ? Decoding
+        value = self.decoding(h)
+        return value
+
+
+class AgnosticDiscreteQNetwork(AgnosticBase):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                 env_list: List[Env],
+                 ):
+        super().__init__(cfg, env_ids, env_list)
+        self.value_decoder: torch.nn.Module = MambaDecoder(cfg)
+
+    def decoding(self, 
+                 num_discretes,
+                 h:torch.Tensor):
+        value = self.value_decoder(num_discretes, h)
+        self.value_weights = value 
+        value = self.out_hidden_op(value, dim=2, keepdim=False) 
+        return value
+        
+    def forward(self, 
+                env: Env,
+                x:torch.Tensor):
+        # x: [batch_size, feature_dim]
+        # ? Encoding
+        h = self.encoding(env, x)
+        # h: [batch_size, d_model]
+        num_discrete = env.action_space.n
+        # ? Decoding
+        value = self.decoding(num_discrete, h)
+        return value
+
+
+class AgnosticContinuousQNetwork(AgnosticBase):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                 env_list: List[Env],
+                 ):
+        super().__init__(cfg, env_ids, env_list)
+        self.act_encoder_1d: torch.nn.Module = MambaDecoder(cfg)
+        self.value_decoder: torch.nn.Module = MambaDecoder(cfg)
+
+    # method overriding
+    def encoding(self,
+                 env: Env,
+                 x: torch.Tensor,
+                 a: torch.Tensor):
+        # ? Encoding from 1D 
+        # x: [batch_size, feature_dim]
+        h = self.obs_encoder_1d(x)
+        a_h = self.act_encoder_1d(a)
+        h = self.in_hidden_op(h, dim=1, keepdim=False)
+        a_h = self.in_hidden_op(a_h, dim=1, keepdim=False)
+        h = h + a_h
+        if self.use_mlp:
+            h = self.act_func(self.res_mlp(h))
+        else:
+            h = self.act_func(h)
+        return h
+    
+    def decoding(self, 
+                 h:torch.Tensor):
+        value = self.value_decoder(1, h)
+        if self.use_transformer:
+            value, value_attn_maps = self.value_transformer(value)
+        self.value_weights = value 
+        value = self.out_hidden_op(value, dim=2, keepdim=False) 
+        return value
+        
+    def forward(self, 
+                env: Env,
+                x:torch.Tensor,
+                a: torch.Tensor):
+        # x: [batch_size, feature_dim]
+        # ? Encoding
+        h = self.encoding(env, x, a)
+        # h: [batch_size, d_model]
+    
+        # ? Decoding
+        value = self.decoding(h)
+        return value
+
+
+class AgnosticContinuousTwinQ(nn.Module):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                 env_list: List[Env],
+                ):
+        super().__init__()
+        self.cq1 = AgnosticContinuousQNetwork(cfg, env_ids, env_list)
+        self.cq2 = AgnosticContinuousQNetwork(cfg, env_ids, env_list)
+
+    def both(self, 
+             env: Env,
+             s:torch.Tensor,
+             a:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.cq1(env, s, a), self.cq2(env, s, a)
+
+    def forward(self, 
+             env: Env,
+             s:torch.Tensor,
+             a:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return torch.min(*self.both(env, s, a))
+    
+        
+class AgnosticDiscreteTwinQ(nn.Module):
+    def __init__(self, 
+                 cfg: OmegaConf,
+                env_ids: List[str], 
+                 env_list: List[Env],
+                ):
+        super().__init__()
+        self.dq1 = AgnosticDiscreteQNetwork(cfg, env_ids, env_list)
+        self.dq2 = AgnosticDiscreteQNetwork(cfg, env_ids, env_list)
+
+    def both(self, 
+             env: Env,
+             s:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.dq1(env, s), self.dq2(env, s)
+
+    def forward(self, 
+             env: Env,
+             s:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return torch.min(*self.both(env, s))
+    
+        
---
+++ projects/rl/pytorch/modules/utils.py
@@ -0,0 +1,360 @@
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import gymnasium as gym
+# from gymnasium.wrappers.record_video import RecordVideo as GymnasiumRecordVideo
+from gymnasium.experimental.wrappers import RecordVideoV0 as GymnasiumRecordVideo
+from gymnasium.spaces import Box as GymnasiumBox
+from gymnasium.spaces import Discrete as GymnasiumDiscrete
+from gymnasium.spaces.dict import Dict as GymnasiumDict
+from typing import Dict, OrderedDict
+import pathlib
+from stable_baselines3.common.atari_wrappers import (  # isort:skip
+    ClipRewardEnv,
+    EpisodicLifeEnv,
+    FireResetEnv,
+    MaxAndSkipEnv,
+    NoopResetEnv,
+)
+from typing import Callable, Tuple, List
+
+def set_seed(seed: int):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    random.seed(seed)
+
+
+gym_envs = [
+'CartPole-v1',
+'MountainCar-v0',
+'MountainCarContinuous-v0',
+'Pendulum-v1',
+'Acrobot-v1',
+'BipedalWalker-v3',
+'BipedalWalkerHardcore-v3',
+'LunarLander-v2',
+'LunarLanderContinuous-v2',
+]
+
+mujoco_envs = [ 
+'Ant-v4',
+'HalfCheetah-v4',
+'Hopper-v4',
+'InvertedDoublePendulum-v4',
+'InvertedPendulum-v4',
+    'Humanoid-v4',
+'HumanoidStandup-v4',
+'Pusher-v4',
+'Reacher-v4',
+'Swimmer-v4',
+'Walker2d-v4',]
+
+
+def get_activation(activation_name: str):
+    if activation_name == 'tanh':
+        activation = nn.Tanh
+    elif activation_name == 'relu':
+        activation = nn.ReLU
+    elif activation_name == 'leakyrelu':
+        activation = nn.LeakyReLU
+    elif activation_name == "prelu":
+        activation = nn.PReLU
+    elif activation_name == 'gelu':
+        activation = nn.GELU
+    elif activation_name == 'sigmoid':
+        activation = nn.Sigmoid
+    elif activation_name in [ None, 'id', 'identity', 'linear', 'none' ]:
+        activation = nn.Identity
+    elif activation_name == 'elu':
+        activation = nn.ELU
+    elif activation_name in ['swish', 'silu']:
+        activation = nn.SiLU
+    elif activation_name == 'softplus':
+        activation = nn.Softplus
+    else:
+        raise NotImplementedError("hidden activation '{}' is not implemented".format(activation_name))
+    return activation
+
+
+class RunningMeanStd:
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, "float64")
+        self.var = np.ones(shape, "float64")
+        self.count = epsilon
+
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count
+        )
+        
+def update_mean_var_count_from_moments(
+    mean, var, count, batch_mean, batch_var, batch_count
+):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
+    return new_mean, new_var, new_count
+
+
+def make_gymnasium_env(env_index, env_id, cfg):
+    capture_video = cfg.experiment.capture_video
+    def thunk():
+        # TODO: Atari
+        # if "Breakout" in env_id:
+        #     env = gym.make(env_id)
+        #     env = gym.wrappers.RecordEpisodeStatistics(env)
+        #     env = NoopResetEnv(env, noop_max=30)
+        #     env = MaxAndSkipEnv(env, skip=4)
+        #     env = EpisodicLifeEnv(env)
+        #     if "FIRE" in env.unwrapped.get_action_meanings():
+        #         env = FireResetEnv(env)
+        #     env = ClipRewardEnv(env)
+        #     env = gym.wrappers.ResizeObservation(env, (84, 84))
+        #     env = gym.wrappers.GrayScaleObservation(env)
+        #     env = gym.wrappers.FrameStack(env, 4)
+        #     return env
+        # else:
+        env = gym.make(env_id, render_mode="rgb_array")
+        if capture_video and env_index == 0:
+            video_path = cfg.paths.video
+            print(video_path)
+            video_path = pathlib.Path(video_path)
+            train_path = pathlib.Path("train")
+            video_save_path = str(video_path / train_path / env_id)
+            print(f"{env_id}: is being recorded")
+            env = GymnasiumRecordVideo(env, video_save_path, disable_logger=True)
+        
+        # env = gym.wrappers.TimeLimit(env, cfg.experiment.max_episode_steps)
+        # env = gym.wrappers.FlattenObservation(env)  # deal with dm_control's Dict observation space
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        env = gym.wrappers.NormalizeObservation(env)
+        env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
+        env = gym.wrappers.NormalizeReward(env, gamma=cfg.ppo.gamma)
+        env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
+        if isinstance(env.action_space, GymnasiumBox):
+            env = gym.wrappers.ClipAction(env)
+
+        return env
+    return thunk
+
+
+class GymnasiumNormalizedFlattenRecordEpisodeStatistics(gym.Wrapper):
+    def __init__(self, env, cfg, deque_size=100):
+        super().__init__(env)
+        if isinstance(env.observation_space, GymnasiumDict) or \
+            isinstance(env.observation_space, Dict) or \
+            isinstance(env.observation_space, OrderedDict):
+            size, highs, lows = self.get_obs_space(env.observation_space)
+            self.observation_space = GymnasiumBox(low=lows, high=highs)
+            # print("observation is changed")
+        self.observation_space = GymnasiumBox(low=env.observation_space.low, high=env.observation_space.high, shape=env.observation_space._shape)
+        print(f"gymnasium env observation space: {env.observation_space}")
+        print(f"gymnasium env observation space type: {type(env.observation_space)}")
+        print(f"gymnasium env action space: {env.action_space}")
+        print(f"gymnasium env action space type: {type(env.action_space)}")
+        self.action_space = GymnasiumDiscrete(env.action_space.n)
+        
+        
+        self.env_type = cfg.experiment.env_type
+        self.num_envs = getattr(env, "num_envs", 1)
+        self.episode_returns = None
+        self.episode_lengths = None
+        self.obs_rms = RunningMeanStd(shape=(self.num_envs, *self.observation_space.shape))
+        self.return_rms = RunningMeanStd(shape=(self.num_envs, ))
+        self.gamma = 0.98
+        self.epsilon = 1e-8
+        
+    def normalize_obs(self, obs):
+        self.obs_rms.update(obs)
+        return (obs - self.obs_rms.mean) / np.sqrt(self.obs_rms.var + self.epsilon)
+    
+    def normalize_rew(self, rews):
+        self.return_rms.update(self.returned_episode_returns)
+        return rews / np.sqrt(self.return_rms.var + self.epsilon)
+        
+    def reset(self, **kwargs):
+        # print(f"Wrapper reset kwargs: {kwargs}")
+        observations, infos = super().reset()
+        if isinstance(observations, Dict) or \
+            isinstance(observations, GymnasiumDict) or \
+                isinstance(observations, OrderedDict):
+            observations =  self.flatten_dict(observations)
+        self.episode_returns = np.zeros(self.num_envs, dtype=np.float32)
+        self.episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
+        self.lives = np.zeros(self.num_envs, dtype=np.int32)
+        self.returned_episode_returns = np.zeros(self.num_envs, dtype=np.float32)
+        self.returned_episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
+        observations = self.normalize_obs(observations)
+        return observations, infos
+
+    def step(self, action):
+        observations, rewards, terminated, truncated, infos = super().step(action)
+        dones = np.logical_or(terminated, truncated)
+        if isinstance(observations, Dict) or \
+            isinstance(observations, GymnasiumDict) or \
+                isinstance(observations, OrderedDict):
+            observations = self.flatten_dict(observations)
+        self.episode_returns += rewards #infos["reward"]
+        self.episode_lengths += 1
+        self.returned_episode_returns[:] = self.episode_returns
+        self.returned_episode_lengths[:] = self.episode_lengths
+        self.episode_returns *= 1 - dones # infos["terminated"]
+        self.episode_lengths *= 1 - dones # infos["terminated"]
+        infos["r"] = self.returned_episode_returns
+        infos["l"] = self.returned_episode_lengths
+        observations = self.normalize_obs(observations)
+        rewards = self.normalize_rew(rewards)
+        return (
+                observations,
+                rewards,
+                terminated,
+                truncated,
+                infos,
+            )
+        
+    def flatten_dict(self, obs):
+        obs_pieces = []
+        for v in obs.values():
+            flat = np.array([v]) if np.isscalar(v) else v.ravel()
+            obs_pieces.append(flat)
+        obs = np.concatenate(obs_pieces, axis=-1)
+        obs = obs.reshape(self.num_envs, -1)
+        return obs
+
+    def get_obs_space(self, observation_space):
+        shapes = []
+        highs = []
+        lows = []
+        for key, box in observation_space.items():
+            if len(box.shape) == 0:
+                shapes.append(1)
+                # highs.append()
+                # lows.append()
+                highs += [*np.expand_dims(box.high, axis=-1)]
+                lows += [*np.expand_dims(box.low, axis=-1)]
+            elif len(box.shape) == 1:
+                shapes += [*box.shape]
+                highs += [*box.high]
+                lows += [*box.low]
+            elif len(box.shape) > 1:
+                # print(box)
+                shapes += [np.prod(box.shape)]
+                highs += [*(box.high.reshape(-1))]
+                lows += [*(box.low.reshape(-1))]
+        # print(shapes)
+        # print(highs)
+        # print(lows)
+        size = (np.sum(shapes, dtype=np.int32))
+        highs = np.array(highs)
+        lows = np.array(lows)        
+        return size, highs, lows
+
+
+class GymNormalizedFlattenRecordEpisodeStatistics(gym.Wrapper):
+    def __init__(self, env, cfg, deque_size=100):
+        super().__init__(env)
+        if isinstance(env.observation_space, GymnasiumDict) or \
+            isinstance(env.observation_space, Dict) or \
+            isinstance(env.observation_space, OrderedDict):
+            size, highs, lows = self.get_obs_space(env.observation_space)
+            self.observation_space = GymnasiumBox(low=lows, high=highs)
+            # print("observation is changed")
+        self.env_type = cfg.experiment.env_type
+        self.num_envs = getattr(env, "num_envs", 1)
+        self.episode_returns = None
+        self.episode_lengths = None
+        self.obs_rms = RunningMeanStd(shape=(self.num_envs, *self.observation_space.shape))
+        self.return_rms = RunningMeanStd(shape=(self.num_envs, ))
+        self.gamma = 0.98
+        self.epsilon = 1e-8
+        
+    def normalize_obs(self, obs):
+        self.obs_rms.update(obs)
+        return (obs - self.obs_rms.mean) / np.sqrt(self.obs_rms.var + self.epsilon)
+    
+    def normalize_rew(self, rews):
+        self.return_rms.update(self.returned_episode_returns)
+        return rews / np.sqrt(self.return_rms.var + self.epsilon)
+        
+    def reset(self, **kwargs):
+        print(f"kwargs: {kwargs}")
+        observations= super().reset(**kwargs)
+        if isinstance(observations, Dict) or \
+            isinstance(observations, GymnasiumDict) or \
+                isinstance(observations, OrderedDict):
+            observations =  self.flatten_dict(observations)
+        self.episode_returns = np.zeros(self.num_envs, dtype=np.float32)
+        self.episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
+        self.lives = np.zeros(self.num_envs, dtype=np.int32)
+        self.returned_episode_returns = np.zeros(self.num_envs, dtype=np.float32)
+        self.returned_episode_lengths = np.zeros(self.num_envs, dtype=np.int32)
+        observations = self.normalize_obs(observations)
+        return observations
+
+    def step(self, action):
+        observations, rewards, dones, infos = super().step(action)
+        if isinstance(observations, Dict) or \
+            isinstance(observations, GymnasiumDict) or \
+                isinstance(observations, OrderedDict):
+            observations = self.flatten_dict(observations)
+        self.episode_returns += rewards #infos["reward"]
+        self.episode_lengths += 1
+        self.returned_episode_returns[:] = self.episode_returns
+        self.returned_episode_lengths[:] = self.episode_lengths
+        self.episode_returns *= 1 - dones # infos["terminated"]
+        self.episode_lengths *= 1 - dones # infos["terminated"]
+        infos["r"] = self.returned_episode_returns
+        infos["l"] = self.returned_episode_lengths
+        observations = self.normalize_obs(observations)
+        rewards = self.normalize_rew(rewards)
+        return (
+                observations,
+                rewards,
+                dones,
+                infos,
+            )
+        
+    def flatten_dict(self, obs):
+        obs_pieces = []
+        for v in obs.values():
+            flat = np.array([v]) if np.isscalar(v) else v.ravel()
+            obs_pieces.append(flat)
+        obs = np.concatenate(obs_pieces, axis=-1)
+        obs = obs.reshape(self.num_envs, -1)
+        return obs
+
+    def get_obs_space(self, observation_space):
+        shapes = []
+        highs = []
+        lows = []
+        for key, box in observation_space.items():
+            if len(box.shape) == 0:
+                shapes.append(1)
+                highs += [*np.expand_dims(box.high, axis=-1)]
+                lows += [*np.expand_dims(box.low, axis=-1)]
+            elif len(box.shape) == 1:
+                shapes += [*box.shape]
+                highs += [*box.high]
+                lows += [*box.low]
+            elif len(box.shape) > 1:
+                shapes += [np.prod(box.shape)]
+                highs += [*(box.high.reshape(-1))]
+                lows += [*(box.low.reshape(-1))]
+        size = (np.sum(shapes, dtype=np.int32))
+        highs = np.array(highs)
+        lows = np.array(lows)        
+        return size, highs, lows
