0ced1962e62b0d3ba71d5c308f3078b9f2706e11
kyhoon <yh01dlx@gmail.com>
2024-07-21 22:23:55 -0700

    feat: add typer and file utils

--- projects/rag/main.py
+++ projects/rag/main.py
@@ -1,21 +1,52 @@
-from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+import typer
+from rich import print
 from source.generator import generate_chain, llm_generator
 from source.retreiver import script_retriever
-from source.utils import get_argumnets, load_yaml
+from source.utils import download_file, list_files, load_env, load_yaml, upload_file
+from typing_extensions import Annotated
 
-if __name__ == "__main__":
+load_env()
+cli = typer.Typer(no_args_is_help=True)
 
-    load_dotenv()
 
-    args = get_argumnets()
-    config = load_yaml(path=args.config_yaml)
+@cli.command()
+def file(
+    mode: Annotated[str, typer.Argument(help="list, upload 또는 download")],
+    src: Annotated[Path, typer.Option("--src", "-s", help="업로드 또는 다운로드할 파일 또는 폴더")] = "./data",
+    dest: Annotated[Path, typer.Option("--dest", "-d", help="다운로드 폴더 경로")] = "./downloads",
+):
+    """파일을 리스트, 업로드 또는 다운로드합니다."""
+    match mode:
+        case "list":
+            print(list_files())
+        case "download":
+            download_file(src, dest)
+        case "upload":
+            upload_file(src)
+        case _:
+            raise NotImplementedError
 
+
+@cli.command()
+def chat(
+    config_yaml: Annotated[str, typer.Option(help="config 파일 위치")] = "config_yaml/base.yaml",
+    update: Annotated[bool, typer.Option("--update")] = False,
+):
+    """챗봇과 대화합니다."""
+    config = load_yaml(path=config_yaml)
     llm = llm_generator(generator_type=config['generator_type'], generator_model=config['generator_model'])
+    db = config["db"]
+    if update:
+        download_file("index.faiss", os.path.join(db, "index.faiss"))
+        download_file("index.pkl", os.path.join(db, "index.pkl"))
 
-    user_input = input("대화를 시작하세요: ")
+    user_input = typer.prompt("대화를 시작하세요")
 
     # Retriever 활용하여 script 불러오기
-    script = script_retriever(config['embedding_type'], config['embedding_model'], config['db'], query=user_input)
+    script = script_retriever(config['embedding_type'], config['embedding_model'], db, query=user_input)
     print(f"검색된 script: {script}")
 
     # 프롬프트 생성
@@ -26,3 +57,7 @@ if __name__ == "__main__":
     answer = generate_chain(llm, prompt_template, prompt_variables)
 
     print(f"AI: {answer}")
+
+
+if __name__ == "__main__":
+    cli()
--- projects/rag/source/utils.py
+++ projects/rag/source/utils.py
@@ -1,23 +1,76 @@
-import argparse
+import os
+from pathlib import Path
 
+import requests
 import yaml
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+from rich import print
 
 
-def load_yaml(path: str):
+def load_env() -> None:
+    load_dotenv()
+    env_variables = [
+        "OPENAI_API_KEY",
+        "FILE_SERVER_URI",
+    ]
+    for var in env_variables:
+        if os.environ.get(var) is None:
+            raise ValueError(f"Environment variable {var} is not set.")
+
+    os.environ["MLFLOW_EXPERIMENT"] = "rag_artifacts"
+
+
+def load_yaml(path: str) -> dict:
     with open(path, 'r', encoding='UTF8') as f:
         load_yaml = yaml.load(f, Loader=yaml.FullLoader)
     return load_yaml
 
 
-def get_argumnets():
-    '''
-    example
-    python train.py --config_yaml base.yaml
-    '''
-    parser = argparse.ArgumentParser()
+def list_files() -> list[str]:
+    url = os.environ["FILE_SERVER_URI"] + "/files"
+    auth = HTTPBasicAuth(
+        "diya2024mamba",
+        "eldi2024akaqk",
+    )
+    response = requests.get(url, auth=auth)
+    response.raise_for_status()
+    return response.json()
 
-    parser.add_argument('--config_yaml', type=str, default='config_yaml/base.yaml')
 
-    args = parser.parse_args()
+def upload_file(path: Path) -> None:
+    files = []
+    if path.is_dir():
+        for f in path.glob("**/*"):
+            if f.is_file():
+                files.append(f)
+    elif path.is_file():
+        files.append(path)
+    if len(files) == 0:
+        print("업로드할 파일이 없습니다.")
+        return
 
-    return args
+    url = os.environ["FILE_SERVER_URI"] + "/upload"
+    auth = HTTPBasicAuth(
+        "diya2024mamba",
+        "eldi2024akaqk",
+    )
+    for f in files:
+        print(f"파일 업로드: {f}")
+        with open(f, 'rb') as handle:
+            response = requests.post(url, files={'file': handle}, auth=auth)
+        response.raise_for_status()
+
+
+def download_file(source: Path, target: Path) -> None:
+    print(f"파일 다운로드: {source} -> {target}")
+    url = os.environ["FILE_SERVER_URI"] + "/download/" + str(source)
+    auth = HTTPBasicAuth(
+        "diya2024mamba",
+        "eldi2024akaqk",
+    )
+    response = requests.get(url, auth=auth)
+    response.raise_for_status()
+
+    with open(target, "wb") as handle:
+        handle.write(response.content)
