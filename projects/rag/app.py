52c1ceed735cf80da4d69a92b9a865b1896fdeed
kyhoon <yh01dlx@gmail.com>
2024-07-28 20:23:10 -0700

    feat: dockerize rag project

--- projects/rag/app.py
+++ projects/rag/app.py
@@ -9,31 +9,41 @@ from source.generator import generate_chain, llm_generator
 from source.retreiver import script_retriever
 from source.utils import load_yaml
 
-if __name__ == "__main__":
+load_dotenv()
 
-    load_dotenv()
+prj_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(prj_dir, 'config_yaml', 'base.yaml')
+config = load_yaml(path=config_path)
 
-    prj_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(prj_dir, 'config_yaml', 'base.yaml')
-    config = load_yaml(path=config_path)
+llm = llm_generator(
+    generator_type=config['generator_type'],
+    generator_model=config['generator_model'],
+)
 
-    llm = llm_generator(generator_type=config['generator_type'], generator_model=config['generator_model'])
 
-    @me.page(
-        path="/text_to_text",
-        title="Text to Text Example",
+def upper_case_stream(query: str):
+    script = script_retriever(
+        config['embedding_type'],
+        config['embedding_model'],
+        config['db'],
+        query,
     )
-    def app():
-        mel.text_to_text(
-            upper_case_stream,
-            title="페르소나 챗봇",
-        )
+    prompt = config['prompt']
+    prompt_variables = {
+        "script": script,
+        "question": query,
+        "character": config['character'],
+    }
+    print(script)
+    answer = generate_chain(llm, prompt, prompt_variables)
+    return answer
 
-    def upper_case_stream(query: str):
-        script = script_retriever(config['embedding_type'], config['embedding_model'], config['db'], query)
 
-        prompt = config['prompt']
-        prompt_variables = {"script": script, "question": query, "character": config['character']}
-        print(script)
-        answer = generate_chain(llm, prompt, prompt_variables)
-        return answer
+@me.page(
+    title="Text to Text Example",
+)
+def app():
+    mel.text_to_text(
+        upper_case_stream,
+        title="페르소나 챗봇",
+    )
