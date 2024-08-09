8fdfde57cd7d055e2a7353d8c2d46dc0d8b1d9b6
chanmuzi <chanmuzi@naver.com>
2024-08-09 23:27:49 +0900

    fix: directory issue

--- projects/llm_eval/eval_llama31.sh
+++ projects/llm_eval/eval_llama31.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-save_dir="eval_results/"
-global_record_file="eval_results/eval_record_collection.csv"
+save_dir="projects/llm_eval/eval_results/"
+global_record_file="projects/llm_eval/eval_results/eval_record_collection.csv"
 model="llama3.1"
 selected_subjects="all"
 gpu_util=0.8
 
 export CUDA_VISIBLE_DEVICES=0
 
-python eval_mmlupro_local.py \
+python projects/llm_eval/eval_mmlupro_local.py \
                  --selected_subjects $selected_subjects \
                  --save_dir $save_dir \
                  --model $model \
