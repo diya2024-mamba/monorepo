ff0022cbaa8d706601358f2cb7eeb9735ff9bb50
hundredeuk <leehd1995@gmail.com>
2024-08-09 09:24:01 +0900

    feature add: mmlu inference with local llm

---
+++ projects/llm_eval/eval_llama31.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+save_dir="eval_results/"
+global_record_file="eval_results/eval_record_collection.csv"
+model="llama3.1"
+selected_subjects="all"
+gpu_util=0.8
+
+export CUDA_VISIBLE_DEVICES=0
+
+python eval_mmlupro_local.py \
+                 --selected_subjects $selected_subjects \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util
+
+
+                
