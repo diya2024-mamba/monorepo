244a57a51d75173cd01a456b746675916b33f33f
kyhoon <yh01dlx@gmail.com>
2024-08-23 20:21:56 -0700

    feat: add random and vote

--- projects/rag/server/server.py
+++ projects/rag/server/server.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import asyncio
+import logging
+import random
 from enum import StrEnum
 
 from chains import base_graph
@@ -26,7 +29,7 @@ class RAG(StrEnum):
     BASE = "base"
 
 
-class Input(BaseModel):
+class InvokeInput(BaseModel):
     llm: LLM
     retriever: Retriever
     rag: RAG
@@ -35,7 +38,7 @@ class Input(BaseModel):
 
 
 @app.post("/invoke")
-async def invoke(input: Input) -> JSONResponse:
+async def invoke(input: InvokeInput) -> JSONResponse:
     match input.llm:
         case LLM.OPENAI:
             llm = ChatOpenAI()
@@ -67,6 +70,86 @@ async def invoke(input: Input) -> JSONResponse:
         }
     )
     return output
+
+
+class RandomInput(BaseModel):
+    character: str
+    prompt: str
+
+
+def random_config() -> tuple[LLM, Retriever, RAG]:
+    llm = random.choice(list(LLM))
+    retriever = random.choice(list(Retriever))
+    rag = random.choice(list(RAG))
+    return llm, retriever, rag
+
+
+@app.post("/random")
+async def random_invoke(input: RandomInput) -> JSONResponse:
+    config1 = random_config()
+    config2 = random_config()
+    while config1 == config2:
+        config2 = random_config()
+
+    input1 = InvokeInput(
+        llm=config1[0],
+        retriever=config1[1],
+        rag=config1[2],
+        character=input.character,
+        prompt=input.prompt,
+    )
+    input2 = InvokeInput(
+        llm=config2[0],
+        retriever=config2[1],
+        rag=config2[2],
+        character=input.character,
+        prompt=input.prompt,
+    )
+
+    outputs = await asyncio.gather(
+        invoke(input1),
+        invoke(input2),
+    )
+    return {
+        "A": {
+            "llm": config1[0],
+            "retriever": config1[1],
+            "rag": config1[2],
+            "output": outputs[0],
+        },
+        "B": {
+            "llm": config2[0],
+            "retriever": config2[1],
+            "rag": config2[2],
+            "output": outputs[1],
+        },
+    }
+
+
+class VoteConfig(BaseModel):
+    llm: LLM
+    retriever: Retriever
+    rag: RAG
+
+
+class VoteInput(BaseModel):
+    winner: VoteConfig
+    loser: VoteConfig
+
+
+# logger for votes
+vote_logger = logging.getLogger("vote_logger")
+file_handler = logging.FileHandler("votes.log")
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter("[VOTE] %(asctime)s - %(message)s")
+file_handler.setFormatter(formatter)
+vote_logger.addHandler(file_handler)
+
+
+@app.post("/vote")
+async def vote(input: VoteInput) -> JSONResponse:
+    vote_logger.info(input.json())
+    return {"message": "투표가 완료되었습니다."}
 
 
 if __name__ == "__main__":
--- projects/rag/server/tests/test_server.py
+++ projects/rag/server/tests/test_server.py
@@ -1,12 +1,12 @@
 import pytest
 from httpx import AsyncClient
-from server import Input, app
+from server import InvokeInput, RandomInput, VoteConfig, VoteInput, app
 
 
 @pytest.mark.parametrize(
     "input",
     [
-        Input(
+        InvokeInput(
             llm="openai",
             retriever="textchunk",
             rag="base",
@@ -16,9 +16,57 @@ from server import Input, app
     ],
 )
 @pytest.mark.asyncio
-async def test_invoke(input: Input) -> None:
+async def test_invoke(input: InvokeInput) -> None:
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.post("/invoke", json=input.model_dump())
 
     assert response.status_code == 200
     assert "generation" in response.json()
+
+
+@pytest.mark.parametrize(
+    "input",
+    [
+        RandomInput(
+            character="해리",
+            prompt="Hello",
+        )
+    ],
+)
+@pytest.mark.asyncio
+async def test_random(input: RandomInput) -> None:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post("/random", json=input.model_dump())
+
+    assert response.status_code == 200
+
+    output = response.json()
+    assert "A" in output
+    assert "llm" in output["A"]
+    assert "generation" in output["A"]["output"]
+
+
+@pytest.mark.parametrize(
+    "input",
+    [
+        VoteInput(
+            winner=VoteConfig(
+                llm="openai",
+                retriever="textchunk",
+                rag="base",
+            ),
+            loser=VoteConfig(
+                llm="openai",
+                retriever="metadata",
+                rag="base",
+            ),
+        )
+    ],
+)
+@pytest.mark.asyncio
+async def test_vote(input: VoteInput) -> None:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post("/vote", json=input.model_dump())
+
+    assert response.status_code == 200
+    assert "message" in response.json()
